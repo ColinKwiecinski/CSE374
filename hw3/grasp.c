4467bd01f14302c8e3cd5406c7636e1e462579d8
colinkwiecinski <colinkwiecinski@gmail.com>
2019-10-29 20:36:55 -0700

    HW3 Submission

--- hw3/grasp.c
+++ hw3/grasp.c
@@ -1,38 +1,72 @@
+#define _GNU_SOURCE
+#include <strings.h>
 #include <string.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 
-int main(int argc, char *argv[argc+1] {
-	int iflag = 0;
-	int cflag = 0;
-	char *cvalue = NULL;
-	int index;
-	int c;
-	opterr = 2;
-
-	// from gnu.org getopt example
-	while ((c = getopt(argc, argv, "ic:")) != -1) {
-		switch (c) {
-		case 'i':
-			iflag = 1;s = 1;
-			break;
-		case 'c':
-			bflag = 1;
-			break;
-		case '?':
-			if (optopt == 'c')
-				fprintf(stderr, "Option -%c requires an argument.\n", optopt);
-			else if (isprint (optopt)
-				fprintf (stderr, "Unknown option `-%c'.\n", optopt);
-			else 
-				fprintf (stderr, Unknown option character `\\x%x'.\n", optopt);
-			return 1;
-		default:
-			abort();
-		}
-
-// make sure to terminate with exit success
+int compare(FILE *file, char *pattern, int iflag, char *file_name) {
+  char buffer[500] = { 0 };
+  int count = 0;
+  while (fgets(buffer, 500, file)) {
+    if (iflag) {
+      if (strcasestr(buffer, pattern) != NULL) {
+        fputs(file_name, stdout);
+        fputs(": ", stdout);
+        fputs(buffer, stdout);
+      }
+    } else if (strstr(buffer, pattern) != NULL) {
+      fputs(file_name, stdout);
+      fputs(": ", stdout);
+      fputs(buffer, stdout);
+    }
+    count++;
+  }
+  return count;
 }
 
+int main(int argc, char *argv[argc + 1]) {
+  int c = 0;
+  int cflag = 0;
+  int iflag = 0;
+  opterr = 2;
+  while ((c = getopt(argc, argv, "ic")) != -1) {
+    switch (c) {
+    case 'i':
+      iflag = 1;
+      break;
+    case 'c':
+      cflag = 1;
+      break;
+    case '?':
+      fprintf(stderr, "Unknown flag given\n");
+      return EXIT_FAILURE;
+    }
+  }
+
+  if (argc - optind < 2) { // check accuracy of conditional
+    fprintf(stderr, "Not enough arguments given\n");
+    return EXIT_FAILURE;
+  }
+  int index = optind;
+  char *pattern = argv[index];
+  index++;
+  int lines = 0;
+  while (index < argc) {
+    char *file_name = argv[index];
+    FILE *file = fopen(file_name, "r");
+    if (file) {
+      lines = lines + compare(file, pattern, iflag, file_name);
+      fclose(file);
+    } else {
+      fprintf(stderr, "Failed to open file\n");
+    }
+
+    index++;
+  }
+  if (cflag) {
+    printf("Total lines scanned: %d\n", lines);
+  }
+  return EXIT_SUCCESS;
+}
